0e0fcf9a3804cf4306fd956a30fbc36febd14462
Luke Tipler <47203384+luketipler@users.noreply.github.com>
2021-03-20 13:27:46 +0000

    Agile sprint 1 completed
    The algorithm now works to detect a person and then another object specified, if not detected will send an audible alert.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# imports initialization
-import sys
-import time
 import cv2
-import numpy
 from playsound import playsound
 
 # from object_detector import *
@@ -10,15 +6,16 @@ from playsound import playsound
 medication_taken = False
 # webcam initialization
 webcamCapture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-webcamCapture.set(3, 1920)
-webcamCapture.set(4, 1080)
+webcamCapture.set(3, 480)
+webcamCapture.set(4, 360)
+# sample files for testing
+# webcamCapture = cv2.VideoCapture("pillTaking1.mp4")
 # coco class names initialization
 classNames = []
 classFile = 'coco.names'
 with open(classFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 print(classNames)
-
 # confidence threshold value
 threshold = 0.6
 class_identifier = []
@@ -26,27 +23,24 @@ box_coordinates = []
 # path locations for object detection
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'frozen_inference_graph.pb'
-# neural network initializations
 # model initialization of neural network for object detection
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
-net.setInputSize(1080, 720)
+net.setInputSize(480, 360)
 net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 # how many times to run through detection loop, change for more loops
-repeat_tolerance = 5
-
-
-# initialize body parts and pairs of said parts
-BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-              "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-              "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-              "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
-POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-              ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-              ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-              ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-              ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
+repeat_tolerance = 10
+detection_tolerance = 5
+# output to mp4 file for testing and demonstration purposes
+output_filename = 'test_output.mp4'
+output_frames_per_second = 20.0
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+file_size = (1080, 720)
+result = cv2.VideoWriter(output_filename,
+                         fourcc,
+                         output_frames_per_second,
+                         file_size)
 
 
 # alert sending function
@@ -56,11 +50,13 @@ def sendAlert(alert_number):
         playsound('test.mp3')
     if alert_number == 2:
         # "time to take your medication"
-        playsound('test.mp3')
+        playsound('')
     if alert_number == 3:
         # "have you taken your medication?"
-        playsound('test.mp3')
+        playsound('')
     # if alert_number == x: , allow for additional sounds.
+
+
 
 
 def objectDetector(n):
@@ -68,7 +64,7 @@ def objectDetector(n):
     for i in range(n):
         success, img = webcamCapture.read()
         class_ids, confidence_values, bounding_box = net.detect(img, threshold)
-        print("Class ID = " + str(class_ids) # + " || ", "Confidence =" + str(confidence_values)
+        print("Class ID = " + str(class_ids)  # + " || ", "Confidence =" + str(confidence_values)
               + "||", "Coordinates " + str(bounding_box))
 
         if len(class_ids) != 0:
@@ -78,57 +74,84 @@ def objectDetector(n):
                             cv2.FONT_ITALIC, 1, (106, 13, 173), 2)
                 class_identifier.append(classId)
                 print(class_identifier)
+                # add the box coordinates to the list.
                 if classId == 1:
                     box_coordinates.append(box)
-                    print(box_coordinates)
+                    # print(box_coordinates)
+
         cv2.imshow('TEST', img)
-        cv2.waitKey(5)
+        result.write(img)
+        cv2.waitKey(10)
 
 
 def poseEstimation(n):
     for i in range(n):
         break
-
-
-def medDetection():
+        
+        
+def medDetection(detection_tolerance):
     print("HEAD")
     objectDetector(repeat_tolerance)
     print(class_identifier)
     # medication bottle class ID
     # checks if any part of the list has the relevant class
-    if any(class_identifier) == 1:
-        print('***************')
-        print(box_coordinates)
-        print('***************')
-        poseEstimation(repeat_tolerance)
+    if 1 in class_identifier:
+        print('|***************************|')
+        print('|      person detected      |')
+        # print(person_coordinates)
+        print('|***************************|')
+        objectDetector(repeat_tolerance)
+        if 77 in class_identifier:
+            global medication_taken
+            medication_taken = True
+            print('|***************************|')
+            print("| Medication has been taken |")
+            # print(box_coordinates)
+            print('|***************************|')
+        else:
+            print("FAIL")
+            if detection_tolerance == 0:
+                sendAlert(1)
+                detection_tolerance = 5
+                # recursive loop until medication is taken
+                medDetection(detection_tolerance)
+            else:
+                detection_tolerance = detection_tolerance - 1
+                medDetection(detection_tolerance)
+        # poseEstimation(repeat_tolerance)
         # left arm point and right arm point
         # left_hand = ''
         # right_hand = ''
         # face_point = ''
         # med_box_coord = ''
-        # if left_hand or right_hand == med_box_coord:
-        # if left_hand or right_hand == face_point:
-        # medication_taken = True
+        # if left_hand or right_hand == med_box_coord: # this is for agile sprint 2
+        # if left_hand or right_hand == face_point: # this is for agile sprint 3
     else:
         print("FAIL")
-        sendAlert(1)
-        # if detection_tolerance == 0:
-        # sendAlert(1)
-        # recursive loop until medication is taken
-        # medDetection()
-        # else:
-        # detection_tolerance = detection_tolerance - 1
-        # medDetection()
-
+        if detection_tolerance == 0:
+            sendAlert(1)
+            detection_tolerance = 5
+            # recursive loop until medication is taken
+            medDetection(detection_tolerance)
+        else:
+            detection_tolerance = detection_tolerance - 1
+            medDetection(detection_tolerance)
 
 def _main_(medication_time):
     if medication_time == True:
+        global medication_taken
         medication_taken = False
-        while medication_taken == False:
-            medDetection()
+        while not medication_taken:
+            medDetection(detection_tolerance)
     else:
         # recursive loop until turned off
         _main_()
 
 
-medDetection()
+def test():
+    while not medication_taken:
+        medDetection(detection_tolerance)
+    result.release()
+
+
+test()
